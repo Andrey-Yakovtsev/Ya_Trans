1acf3b0a3b7432849a0f915b1aa455a65d2954e9
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-06-01 13:18:04 +0300

    Task 1 - Translation. Done.

--- V1_5_example.py
+++ V1_5_example.py
@@ -1,38 +1,73 @@
 import requests
-import json
-
-'''
-Необходимо расширить функцию переводчика так, чтобы она принимала следующие параметры:
-
-Путь к файлу с текстом;
-Путь к файлу с результатом;
-Язык с которого перевести;
-Язык на который перевести (по-умолчанию русский).
-'''
-
+from pprint import pprint
 #  документация https://yandex.ru/dev/translate/doc/dg/reference/translate-docpage/
 
-API_KEY = 'AQVN08OIYdMF8D5rGIFOH-Xm5FnJYEd07nKIPjJP'
-FOLDER_ID = 'ajejoe463ko5u0v6m641'
+API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
-def translate_it(text, to_lang): #initial_file_name, result_file_name, from_lang
+
+
+def translate_it(inputfile, outputfile, to_lang:'ru'):
+    with open(inputfile, encoding='utf-8') as initial_file:
+        news = initial_file.read()
+
+        params = {
+            'key': API_KEY,
+            'text': news,
+            'lang': 'ru',  #"'ru-{}'.format(to_lang)
+            'options': 1
+        }
+
+        response = requests.get(URL, params=params)
+        json_ = response.json()
+        pprint(json_['lang'])
+        line = ''.join(json_['text'])
+
+    with open (outputfile, 'w') as file_output:
+        file_output.write(line)
+
+
+translate_it('DE.txt', 'DE-RU.txt', 'ru')
+translate_it('ES.txt', 'ES-RU.txt', 'ru')
+translate_it('FR.txt', 'FR-RU.txt', 'ru')
+
+
+'''
+Исходная версия
+
+
+def translate_it(text, to_lang):
+    """
+    https://translate.yandex.net/api/v1.5/tr.json/translate ?
+    key=<API-ключ>
+     & text=<переводимый текст>
+     & lang=<направление перевода>
+     & [format=<формат текста>]
+     & [options=<опции перевода>]
+     & [callback=<имя callback-функции>]
+    :param to_lang:
+    :return:
+    """
 
     params = {
-    'key': API_KEY,
-    'texts': ['Переводи давай'],
-    'format': 'plain',
-    'lang': 'ru-en'
+        'key': API_KEY,
+        'text': line,
+        'lang': 'ru',  #"'ru-{}'.format(to_lang)
+        'options': 1
     }
 
-    response = requests.post(URL, params=params)
-    print(response)
+    response = requests.get(URL, params=params)
     json_ = response.json()
-    print(response.json())
-    # return ''.join(json_['text'])
+    pprint(json_['lang'])
+    return ''.join(json_['text'])
 
 
-# print(translate_it('В настоящее время доступна единственная опция — признак включения в ответ автоматически определенного языка переводимого текста. Этому соответствует значение 1 этого параметра.', 'no'))
+
 
 if __name__ == '__main__':
-    print(translate_it('А ну-ка переведи мне текст! Быстро!', 'en'))
+    with open('DE.txt', encoding='utf-8') as initial_file:
+        line = initial_file.read()
+    with open ('DE-RU.txt', 'w') as file_output:
+        file_output.write(translate_it('line', 'ru'))
+
+'''
