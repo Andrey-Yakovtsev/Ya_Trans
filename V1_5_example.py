386bed95a27efee4e9a78d282d039f42f5fa0fb0
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-06-01 16:32:03 +0300

    Task 2 - Ya.Disk.Upload. Done.

--- V1_5_example.py
+++ V1_5_example.py
@@ -1,20 +1,19 @@
 import requests
 from pprint import pprint
+import os
+import json
 #  документация https://yandex.ru/dev/translate/doc/dg/reference/translate-docpage/
 
-API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
-URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-
-
-
 def translate_it(inputfile, outputfile, to_lang:'ru'):
+    API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
+    URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
     with open(inputfile, encoding='utf-8') as initial_file:
         news = initial_file.read()
 
         params = {
             'key': API_KEY,
             'text': news,
-            'lang': 'ru',  #"'ru-{}'.format(to_lang)
+            'lang': 'ru',
             'options': 1
         }
 
@@ -27,47 +26,33 @@ def translate_it(inputfile, outputfile, to_lang:'ru'):
         file_output.write(line)
 
 
-translate_it('DE.txt', 'DE-RU.txt', 'ru')
-translate_it('ES.txt', 'ES-RU.txt', 'ru')
-translate_it('FR.txt', 'FR-RU.txt', 'ru')
+def send_it_to_disk(filename):
 
+    DISKURL = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
 
-'''
-Исходная версия
-
-
-def translate_it(text, to_lang):
-    """
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-    :param to_lang:
-    :return:
-    """
-
-    params = {
-        'key': API_KEY,
-        'text': line,
-        'lang': 'ru',  #"'ru-{}'.format(to_lang)
-        'options': 1
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': 'OAuth AgAAAAA7LlbAAADLW2uUXegQwkg8qje0HgTqcEo'
     }
 
-    response = requests.get(URL, params=params)
-    json_ = response.json()
-    pprint(json_['lang'])
-    return ''.join(json_['text'])
+    params = {
+        'path': os.path.join(filename),
+        'overwrite': 'True'
+    }
 
+    response = requests.get(DISKURL, headers=headers, params=params)
+    upload_url = response.json()['href']
+    files = {'file': open(filename, 'rb')}
+    upload_response = requests.put(upload_url, files=files)
+    print(upload_response.status_code)
+    files['file'].close()
 
+translate_it('DE.txt', 'DE-RU.txt', 'ru')
+send_it_to_disk('DE-RU.txt')
 
+translate_it('FR.txt', 'FR-RU.txt', 'ru')
+send_it_to_disk('FR-RU.txt')
 
-if __name__ == '__main__':
-    with open('DE.txt', encoding='utf-8') as initial_file:
-        line = initial_file.read()
-    with open ('DE-RU.txt', 'w') as file_output:
-        file_output.write(translate_it('line', 'ru'))
+translate_it('ES.txt', 'ES-RU.txt', 'ru')
+send_it_to_disk('ES-RU.txt')
 
-'''
